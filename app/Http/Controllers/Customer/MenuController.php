e53acc19d50d08c4d8c9e8829ec623ac45beabd9
Ayayink <247006111197@student.unsil.ac.id>
2026-05-01 22:29:36 +0700

    revisi menu

--- app/Http/Controllers/Customer/MenuController.php
+++ app/Http/Controllers/Customer/MenuController.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\Controller;
 
 class MenuController extends Controller
 {
-<<<<<<< HEAD
     public function index(Request $request)
     {
         $categories = Category::all();
@@ -53,15 +52,6 @@ class MenuController extends Controller
         }
 
         $products = $query->get();
-=======
-    public function index()
-    {
-        $categories = Category::all();
-        $products = Product::with('category')
-            ->where('stock', '>', 0)
-            ->orderBy('name')
-            ->get();
->>>>>>> d07f41bcb216716dc31f54d38658c83380117c94
 
         return view('customer.pages.menu', compact('categories', 'products'));
     }
